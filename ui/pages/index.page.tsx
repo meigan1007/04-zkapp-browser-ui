f2a06761bf6ccbec7114f91668032415623bf606
meigan <1218416686@qq.com>
2022-11-23 16:59:21 +0800

    update

--- ui/pages/index.page.tsx
+++ ui/pages/index.page.tsx
@@ -20,14 +20,13 @@ export default function Home() {
       // Update this to use the address (public key) for your zkApp account
       // To try it out, you can try this address for an example "Add" smart contract that we've deployed to 
       // Berkeley Testnet B62qqkb7hD1We6gEfrcqosKt9C398VLp1WXeTo1i9boPoqF7B1LxHg4
-      const zkAppAddress = '';
+      const zkAppAddress = 'B62qqkb7hD1We6gEfrcqosKt9C398VLp1WXeTo1i9boPoqF7B1LxHg4';
       // This should be removed once the zkAppAddress is updated.
       if (!zkAppAddress) {
         console.error(
           'The following error is caused because the zkAppAddress has an empty string as the public key. Update the zkAppAddress with the public key for your zkApp account, or try this address for an example "Add" smart contract that we deployed to Berkeley Testnet: B62qqkb7hD1We6gEfrcqosKt9C398VLp1WXeTo1i9boPoqF7B1LxHg4'
         );
       }
-
       const zkAppInstance = new Add(PublicKey.fromBase58(zkAppAddress));
       
     })();
